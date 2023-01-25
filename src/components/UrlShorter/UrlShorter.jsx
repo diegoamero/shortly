71a32e96c0fa4b115ac2581065bf96f2019c23d0
Diego Mejia <diegoamejia.10@gmail.com>
2023-01-24 21:56:37 -0400

    Fix header design and Add Url shorter box

--- src/components/UrlShorter/UrlShorter.jsx
+++ src/components/UrlShorter/UrlShorter.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './UrlShorter.scss'
 
 function UrlShorter() {
     return (
         <div className="url-shorter--container">
             <form className="url-shorter--form">
-                <input type="text" placeholder="Shorten a link here..." />
-                <input type="submit" value="Shorten It!" />
+                <input className="form--url" type="text" placeholder="Shorten a link here..." />
+                <input className="form--button" type="submit" value="Shorten It!" />
             </form>
             <div className="urls--container"></div>
         </div>
